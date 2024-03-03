83a17e398211a6dec1921e425674bbf47f154e14
[Srilaxmi Danthuluri] <[meesubrah@gmail.com]>
2024-03-03 13:37:57 +0000

    created a degree celsius tokelvin function

--- temp_convertor.py
+++ temp_convertor.py
@@ -1,3 +1,6 @@
+def celsius_to_kelvin(temp):
+  return temp + 273.15  
+
 temp = float(input('Enter the temperature in degrees celsius:'))
-temp_kelvin = temp + 273.15
+temp_kelvin = celsius_to_kelvin(temp)
 print(f'Your Temperature in Kelvin is:{temp_kelvin}')
